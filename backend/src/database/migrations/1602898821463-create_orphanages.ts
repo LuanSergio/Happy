6bfbc44fb2c8688863346a53bd58537c70931616
Luan <luan.sergio@novahaus.com.br>
2020-10-17 16:57:23 -0300

    working on image upload

--- backend/src/database/migrations/1602898821463-create_orphanages.ts
+++ backend/src/database/migrations/1602898821463-create_orphanages.ts
@@ -39,6 +39,10 @@ export class createOrphanages1602898821463 implements MigrationInterface {
           type: 'text',
         },
         {
+          name: 'opening_hours',
+          type: 'varchar'
+        },
+        {
           name: 'open_on_weekends',
           type: 'boolean',
           default: false,
@@ -48,6 +52,7 @@ export class createOrphanages1602898821463 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('orphanages');
   }
 
 }
