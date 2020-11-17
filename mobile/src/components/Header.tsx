2b77b6b246bf87c185b838bdf60a0ee37c18e8dd
Luan Sergio <luansergiomattos@gmail.com>
2020-11-17 19:44:10 -0300

    working on mobile

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -1,6 +1,8 @@
 import { useLinkProps } from '@react-navigation/native';
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import { BorderlessButton } from 'react-native-gesture-handler';
+import { Feather } from '@expo/vector-icons'
 
 interface HeaderProps {
   title: string;
@@ -9,7 +11,14 @@ interface HeaderProps {
 export default function Header({ title }: HeaderProps) {
   return (
     <View style={styles.container}>
+      <BorderlessButton onPress={() => { }}>
+        <Feather name="arrow-left" size={24} color="#15b6d6" />
+      </BorderlessButton>
       <Text style={styles.title}>{title}</Text>
+
+      <BorderlessButton onPress={() => { }}>
+        <Feather name="x" size={24} color="#ff669d" />
+      </BorderlessButton>
     </View>
   )
 }
--- mobile/src/pages/OrphanagesMap.tsx
+++ mobile/src/pages/OrphanagesMap.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native'
 
 import mapMarker from '../images/map-marker.png';
+import { RectButton } from 'react-native-gesture-handler';
 
 export default function OrpahanagesMap() {
   const navigation = useNavigation();
@@ -51,9 +52,9 @@ export default function OrpahanagesMap() {
           2 orfanatos encontrado
         </Text>
 
-        <TouchableOpacity style={styles.createOrphanageButton} onPress={handleNavigateToCreateOrphanage}>
+        <RectButton style={styles.createOrphanageButton} onPress={handleNavigateToCreateOrphanage}>
           <Feather name="plus" size={20} color="#FFF" />
-        </TouchableOpacity>
+        </RectButton>
       </View>
     </View>
   );
