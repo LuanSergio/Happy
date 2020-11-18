5b4453ac613b6f794fe79517b3fe1bb53c7867fc
Luan Sergio <luansergiomattos@gmail.com>
2020-11-18 20:03:22 -0300

    working on mobile

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { useLinkProps } from '@react-navigation/native';
+import { useLinkProps, useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import { BorderlessButton } from 'react-native-gesture-handler';
@@ -6,19 +6,30 @@ import { Feather } from '@expo/vector-icons'
 
 interface HeaderProps {
   title: string;
+  showCancel?: boolean;
 }
 
-export default function Header({ title }: HeaderProps) {
+export default function Header({ title, showCancel = true }: HeaderProps) {
+  const navigation = useNavigation();
+
+  function handleGoBackToAppHomepage() {
+    navigation.navigate('OrphanagesMap');
+  }
+
   return (
     <View style={styles.container}>
-      <BorderlessButton onPress={() => { }}>
+      <BorderlessButton onPress={navigation.goBack}>
         <Feather name="arrow-left" size={24} color="#15b6d6" />
       </BorderlessButton>
       <Text style={styles.title}>{title}</Text>
 
-      <BorderlessButton onPress={() => { }}>
-        <Feather name="x" size={24} color="#ff669d" />
-      </BorderlessButton>
+      { showCancel ? (
+        <BorderlessButton onPress={handleGoBackToAppHomepage}>
+          <Feather name="x" size={24} color="#ff669d" />
+        </BorderlessButton>
+      ) : (
+          <View />
+        )}
     </View>
   )
 }
