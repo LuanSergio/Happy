3b53a0c1ef80bc08b09c04c9a48e23a5e37d3237
Luan Sergio <luansergiomattos@gmail.com>
2020-12-11 22:58:33 -0300

    backend adjusts to deploy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express from 'express';
 import path from 'path';
 import 'express-async-errors';
@@ -16,4 +17,4 @@ app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 app.use(errorHandler);
 
-app.listen(3333);
+app.listen(process.env.PORT || 3333);
